3b1862152754112d370f8eb2e93aa5ff360a2ce9
humza <humza@LAPTOP-AL5E1Q56>
2020-11-09 16:26:25 +0530

    First Commit

---
+++ CoreJava/src/com/core/Multiply.java
@@ -0,0 +1,16 @@
+package com.core;
+
+public class Multiply {
+
+	public static void main(String[] args) {
+		int a=2,b=14;
+		int resul=0;
+		
+		while(b!=0) {
+			resul= resul+a;
+			b--;	
+		}
+		System.out.println(resul);	
+	}
+
+}
